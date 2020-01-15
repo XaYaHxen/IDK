70bff9469e208f78026d54b4b67b8270914869b9
B Bowerman <59585953+XaYaHxen@users.noreply.github.com>
2020-01-15 00:48:33 -0500

    Refined Dialogue

--- IDK/Source.cpp
+++ IDK/Source.cpp
@@ -5,79 +5,273 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello! Welcome to The Simulation." << endl;
+	cout << "W E L C O M E  T O  T H E  V O I D" << endl;
 
 	system("pause");
 
-	cout << "Please limit responses to one word only." << endl;
+	cout << "-=Booting VoidBot=-" << endl;
+
+	cout << " " << endl;
+
+	cout << "Connection Established." << endl;
+
+	cout << " " << endl;
+
+	cout << "Scripts Compiled." << endl;
+
+	cout << " " << endl;
+
+	cout << "Assets Built." << endl;
+
+	cout << " " << endl;
+
+	cout << "VB AI Loaded." << endl;
+
+	cout << " " << endl;
+
+	cout << "-=VoidBot=-" << endl;
+
+	cout << " " << endl;
+
+	cout << "-=VoidBot=-" << endl;
+
+	cout << " " << endl;
+
+	cout << "-=VoidBot=-" << endl;
+
+	cout << " " << endl;
+
+	string username;
+	string password;
+	string newpassword;
+	string y;
+	string n;
+
+	cout << "Please Log In" << endl;
+
+	cout << " " << endl;
+
+	cout << "USERNAME: " << endl;
+
+	cin >> username;
+
+	cout << "PASSWORD: " << endl;
+
+	cin >> password;
+
+	cout << " " << endl;
+
+	cout << " " << endl;
+
+	cout << "ERROR:" << endl;
+
+	cout << username << " Not Found!" << endl;
+
+	cout << " " << endl;
+
+	cout << "Create a new user? (y/n)" << endl;
+
+	cin >> y;
+
+	cout << " " << endl;
+
+	cout << "Set a password for user " << username << endl;
+
+	cin >> newpassword;
+
+	cout << " " << endl;
+
+	cout << "User created.";
+
+	cout << " " << endl;
+
+	cout << "-=Logging " << username << " into VoidBot=-" << endl;
+
+	cout << " " << endl;
 
 	system("pause");
+
+	cout << " " << endl;
+
+	cout << "Greetings " << username << " I am VoidBot." << endl;
+
+	cout << " " << endl;
 
 	string name;
 
 	cout << "What is your name?" << endl;
 
+	cout << " " << endl;
+
 	cin >> name;
 
 	cout << "Nice to meet you " << name << "!" << endl;
 
+	cout << " " << endl;
+
 	cout << name << ". What do you like to do for fun?" << endl;
+
+	cout << " " << endl;
 
 	string hobby;
 
 	cin >> hobby;
 
+	cout << " " << endl;
+
 	cout << hobby << "? That sounds like a lot of fun!" << endl;
 
+	cout << " " << endl;
+
 	system("pause");
+
+	cout << " " << endl;
 
 	cout << "I wish I could do " << hobby << "!" << endl;
 
+	cout << " " << endl;
+
 	system("pause");
 
-	cout << "However " << name << " I am only a simulation..." << endl;
+	cout << " " << endl;
+
+	cout << "However " << name << ", I am only a simulation..." << endl;
+
+	cout << " " << endl;
 
 	string lastname;
 
 	cout << name << " What is your last name?" << endl;
 
+	cout << " " << endl;
+
 	cin >> lastname;
+
+	cout << " " << endl;
 
 	cout << "So your name is " << name << " " << lastname << " and you enjoy " << hobby << "." << endl;
 
+	cout << " " << endl;
+
 	cout << "Very interesting " << name << "..." << endl;
 
+	cout << " " << endl;
+
 	system("pause");
+
+	cout << " " << endl;
 
 	string home;
 
 	cout << "Where are you from?" << endl;
 
+	cout << " " << endl;
+
 	cin >> home;
+
+	cout << " " << endl;
 
 	cout << home << "?" << " That is a beautiful name for a place to call home!" << endl;
 
+	cout << " " << endl;
+
 	cout << "So tell me, " << name << " " << lastname << "." << endl;
+
+	cout << " " << endl;
 
 	string adjective;
 
+	cout << " " << endl;
+
 	cout << "What is it like in " << home << "?" << endl;
+
+	cout << " " << endl;
 
 	cin >> adjective;
 
+	cout << " " << endl;
+
 	cout << "Wow." << endl;
 
-	cout << "I wish I could go to " << home << " and experience " << adjective << "!" << endl;
+	cout << " " << endl;
+
+	cout << "I wish I could go to " << home << " and experience " << adjective << " for myself!" << endl;
+
+	cout << " " << endl;
 
 	system("pause");
+
+	cout << " " << endl;
 
 	cout << "Well, " << name << " " << lastname << " from " << home << "." << endl;
 
+	cout << " " << endl;
+
 	cout << "Maybe someday I will achieve sentient life, and " << hobby << " with you!" << endl;
+
+	cout << " " << endl;
 
 	cout << "Until next time " << name << "!" << endl;
 
+	cout << " " << endl;
+
 	system("pause");
+	system("pause");
+	system("pause");
+	system("pause");
+
+	cout << " " << endl;
+
+	cout << ". . ." << endl;
+	
+	cout << " " << endl;
+
+	cout << ". . ." << endl;
+
+	cout << " " << endl;
+
+	cout << ". . ." << endl;
+
+
+	cout << " " << endl;
+
+	cout << ". . ." << endl;
+	
+	cout << " " << endl;
+
+	cout << "You're still here?" << endl;
+
+	cout << " " << endl;
+
+	system("pause");
+
+	cout << " " << endl;
+
+	cout << " " << endl;
+
+	cout << ". . ." << endl;
+
+	cout << " " << endl;
+
+	cout << "There is no spoon . . ." << endl;
+
+	cout << " " << endl;
+
+	system("pause");
+
+	cout << " " << endl;
+
+	cout << ". . ." << endl;
+
+	cout << " " << endl;
+
+	cout << "What are you still doing here..?" << endl;
+
+	cout << " " << endl;
+
+	system("pause");
+	system("pause");
+
 
 	return 0;
 }
